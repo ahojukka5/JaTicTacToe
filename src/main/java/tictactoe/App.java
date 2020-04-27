fb7a91fa8d9a7b7604d4ab2abae5b4a6e034b676
Jukka Aho <ahojukka5@gmail.com>
2020-04-28 03:46:09 +0700

    Fix styles in App.java

--- src/main/java/tictactoe/App.java
+++ src/main/java/tictactoe/App.java
@@ -11,22 +11,47 @@ import javafx.scene.layout.GridPane;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
-public class App extends Application {
-
-    private static final char[] playerMarks = new char[]{'_', 'X', 'O'};
-    private transient final Label header = new Label("Turn: X");
-    private transient final Board board = new Board();
+/**
+ * TicTacToe main application.
+ */
+public final class App extends Application {
 
     /**
-     * Return the current player mark, either "X" or "O"
+     * Player marks, by default first player is X and second is O.
+     */
+    private static final char[] MARKS = new char[] {'_', 'X', 'O'};
+
+    /**
+     * Text shown in the top of the game.
+     */
+    private final transient Label header = new Label("Turn: X");
+
+    /**
+     * Font used in header.
+     */
+    private final transient Font headerFont = Font.font("Monospaced", 20);
+
+    /**
+     * Game board.
+     */
+    private final transient Board board = new Board();
+
+    /**
+     * Font used in buttons.
+     */
+    private final transient Font buttonFont = Font.font("Monospaced", 40);
+
+    /**
+     * Return the current player mark, either "X" or "O".
+     *
      * @return String "X" or "O"
      */
     private String getCurrentPlayerMark() {
-        return String.valueOf(playerMarks[board.getCurrentPlayer()]);
+        return String.valueOf(MARKS[board.getCurrentPlayer()]);
     }
 
     /**
-     * Updates header, showing turn 'X' / 'O' or "End of Game"
+     * Updates header, showing turn 'X' / 'O' or "End of Game".
      */
     private void updateHeader() {
         if (board.endOfGame()) {
@@ -41,24 +66,45 @@ public class App extends Application {
      */
     private class ButtonClickHandler implements EventHandler<ActionEvent> {
 
-        private transient final Button button;
-        private transient boolean buttonPressed = false;
-        private transient final int i;
-        private transient final int j;
+        /**
+         * Button which this handler is registered to.
+         */
+        private final transient Button btn;
 
-        public ButtonClickHandler(Button button, int i, int j) {
-            this.button = button;
-            this.i = i;
-            this.j = j;
+        /**
+         * Contains value is this button already pressed.
+         */
+        private transient boolean buttonPressed = false;
+
+        /**
+         * Board row location for this button, 0 is first.
+         */
+        private final transient int i;
+
+        /**
+         * Board column location for this button, 0 is first.
+         */
+        private final transient int j;
+
+        /**
+         * Initialized new handler for button.
+         * @param button button
+         * @param row row location of button in game board
+         * @param col column location of button in game board
+         */
+        ButtonClickHandler(final Button button, final int row, final int col) {
+            this.btn = button;
+            this.i = row;
+            this.j = col;
         }
 
         @Override
-        public void handle(ActionEvent event) {
+        public void handle(final ActionEvent event) {
             if (buttonPressed || board.endOfGame()) {
                 return;
             }
             buttonPressed = true;
-            button.setText(App.this.getCurrentPlayerMark());
+            btn.setText(App.this.getCurrentPlayerMark());
             board.setState(i, j, board.getCurrentPlayer());
             board.nextPlayer();
             updateHeader();
@@ -66,29 +112,33 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) {
+    public void start(final Stage stage) {
 
-        header.setFont(Font.font("Monospaced", 20));
-        GridPane gridPane = new GridPane();
-        BorderPane borderPane = new BorderPane();
+        header.setFont(headerFont);
+        final GridPane gridPane = new GridPane();
+        final BorderPane borderPane = new BorderPane();
         borderPane.setTop(header);
         borderPane.setCenter(gridPane);
 
-        for (int i = 1; i <= 3; i++) {
-            for (int j = 1; j <= 3; j++) {
-                Button btn = new Button(" ");
-                btn.setFont(Font.font("Monospaced", 40));
+        for (int i = 1; i <= board.getNumberOfRows(); i++) {
+            for (int j = 1; j <= board.getNumberOfColumns(); j++) {
+                final Button btn = new Button(" ");
+                btn.setFont(buttonFont);
                 btn.setOnAction(new ButtonClickHandler(btn, i - 1, j - 1));
                 gridPane.add(btn, i, j);
             }
         }
 
-        Scene mainScene = new Scene(borderPane);
+        final Scene mainScene = new Scene(borderPane);
         stage.setScene(mainScene);
         stage.show();
     }
 
-    public static void main(String[] args) {
-        launch(App.class);
+    /**
+     * Run the main application.
+     * @param args command line arguments to game
+     */
+    public static void main(final String[] args) {
+        launch(args);
     }
 }
