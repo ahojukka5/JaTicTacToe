69a05e266e0c04c440c3a8d0c2931b178750e204
Jukka Aho <ahojukka5@gmail.com>
2020-04-28 03:46:32 +0700

    Fix styles in BoardTest.java

--- src/test/java/tictactoe/BoardTest.java
+++ src/test/java/tictactoe/BoardTest.java
@@ -1,9 +1,17 @@
 package tictactoe;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 import org.junit.Test;
-import static org.junit.Assert.*;
 
+/**
+ * Game board tests.
+ */
 public class BoardTest {
+
+    /**
+     * Test that initial state of the board is ok.
+     */
     @Test public void testBoardInitialState() {
         Board board = new Board();
         assertTrue(!board.endOfGame());
